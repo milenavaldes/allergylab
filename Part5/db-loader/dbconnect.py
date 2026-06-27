86f29df3624f19f092632e395efca7f00b24d1d1
Milena Valdes <milenavaldes1@gmail.com>
2026-06-27 18:30:03 -0400

    new commit

--- Part5/db-loader/dbconnect.py
+++ Part5/db-loader/dbconnect.py
@@ -1,12 +1,16 @@
 import mysql.connector
+import os
+db_name = os.environ.get('DB_NAME')
+db_user = os.environ.get('DB_USER')
+db_password = os.environ.get('DB_PASSWORD')
 
 def connection():
     # Edited out actual values
-    conn = mysql.connector.connect( host='dbhost',
+    conn = mysql.connector.connect(host='dbhost',
                             port=3306,
-                            database='allergen_info',
-                            user='pythonapp',
-                            password='secretPass123!')
+                            database=db_name,
+                            user=db_user,
+                            password=db_password)
     c = conn.cursor()
 
     return c, conn
